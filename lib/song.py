54ab32ce0ed3ea627acd9af727d1244f2c5ad83d
jeff <jeff.etale@student.moringaschool.com>
2023-08-26 14:52:42 +0300

    done lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,47 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre) -> None:
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+    
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
+    
+    def add_to_artists(self):
+        if self.artist not in Song.artists:
+            Song.artists.append(self.artist)
+    
+    def add_to_genre_count(self):
+        if self.genre not in Song.genre_count:
+            Song.genre_count[self.genre] = 1
+        else:
+            Song.genre_count[self.genre] += 1
+    
+    def add_to_artist_count(self):
+        if self.artist not in Song.artist_count:
+            Song.artist_count[self.artist] = 1
+        else:
+            Song.artist_count[self.artist] += 1
+
+
+'''
+song1 = Song("Song 1", "Artist 1", "Genre 1")
+song2 = Song("Song 2", "Artist 2", "Genre 2")
+song3 = Song("Song 3", "Artist 1", "Genre 1")
+
+print("Song count:", Song.count)
+print("Genres:", Song.genres)
+print("Artists:", Song.artists)
+'''
